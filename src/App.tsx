a6ec87ec3d115035572851d797184e5ce7c020ce
vishesh srivastava <visheshsri0028@gmail.com>
2023-05-03 12:37:20 +0530

    First React Project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
+
+function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
+    </div>
+  );
+}
+
+export default App;
